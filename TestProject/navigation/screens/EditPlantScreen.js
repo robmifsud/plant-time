e90d8a703ce22e0140fcdcaa2fc29396d38a61e7
Luke Chen <lukechen67@gmail.com>
2023-01-06 11:16:20 +0100

    Minor styling change

--- TestProject/navigation/screens/EditPlantScreen.js
+++ TestProject/navigation/screens/EditPlantScreen.js
@@ -51,6 +51,13 @@ export default function AddPlantsScreen({ navigation }) {
 					</View>
 				</TouchableOpacity>
 
+				<TouchableOpacity onPress={''} style={styles.buttonClickContain}>
+					<View style={styles.button}>
+						<Icon name='thermometer-1' size={25} style={styles.icon} />
+						<Text style={styles.buttonText}>Temp_1</Text>
+					</View>
+				</TouchableOpacity>
+
 				<Image
 					style={styles.doneImage}
 					source={require('../../assets/images/done-icon.png')}
